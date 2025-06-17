38fb9993f46d4f71f7f837099d39ae716b7bb200
albinlundberg <albinlundberg@users.noreply.github.com>
2025-06-17 19:53:37 +0200

    Moved Models to Domain layer

--- API/Program.cs
+++ API/Program.cs
@@ -7,38 +7,30 @@ namespace API
         {
            var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            // Add services to the container.
 
-// builder.Services.AddIdentity<User, IdentityRole>()
-//     .AddEntityFrameworkStores<ApplicationDbContext>()
-//     .AddDefaultTokenProviders();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllersWithViews();
+            var app = builder.Build();
 
-var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+            app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+            app.UseAuthorization();
 
-app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
+            app.MapControllers();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+            app.Run();
         }
     }
 }
