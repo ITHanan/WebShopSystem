74e5a947e42ba1205363781ca96ec6693d8816ee
albinlundberg <albinlundberg@users.noreply.github.com>
2025-06-17 12:42:07 +0200

    Edit Models

--- ApplicationLayer/Models/Course.cs
+++ ApplicationLayer/Models/Course.cs
@@ -10,7 +10,12 @@ namespace ApplicationLayer.Models
         public int CourseID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Enum Level { get; set; }
+        public enum CourseLevel
+        {
+            Beginner,
+        Intermediate,
+        Advanced
+        }
         public string Language { get; set; }
         public DateTime Time { get; set; }
         public DateTime StartDate { get; set; }
