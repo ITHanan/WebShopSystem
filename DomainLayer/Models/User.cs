ccf7806cb14654a2a28625b69b141c28c7ccc939
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-06-19 18:02:03 +0200

    Make PhoneNumber (Nullable in DB)

--- DomainLayer/Models/User.cs
+++ DomainLayer/Models/User.cs
@@ -12,7 +12,7 @@ namespace DomainLayer.Models
         public string? UserName { get; set; }
         [EmailAddress]
         public string? Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string?PhoneNumber { get; set; }
         [Required]
         public string PasswordHash { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
