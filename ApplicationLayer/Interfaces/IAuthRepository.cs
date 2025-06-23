ee908266a901922bd0e6470efb01f24c17efca3f
albinlundberg <albinlundberg@me.com>
2025-06-23 11:35:43 +0200

    Typo

--- ApplicationLayer/Interfaces/IAuthRepository.cs
+++ ApplicationLayer/Interfaces/IAuthRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ApplicationLayer.Interfaces5
+namespace ApplicationLayer.Interfaces
 {
     public interface IAuthRepository
     {
