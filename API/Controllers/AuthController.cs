697ce9b89a594f9c4ce32ff4cd92fd2b284a6fe2
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-06-28 12:51:05 +0200

    adding UseCore in program.cs

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -20,7 +20,7 @@ namespace API.Controllers
 
 
         [AllowAnonymous]
-        [HttpPost("register")]
+        [HttpPost("SignUp")]
         public async Task<IActionResult> Register([FromBody] UserRegisterDto userRegisterDto)
         {
             var command = new RegisterCommand(userRegisterDto.UserName, userRegisterDto.UserEmail, userRegisterDto.Password);
